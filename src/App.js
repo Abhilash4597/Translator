302cff3f9c2dc1a6b26b385e250f8bf3958cc1c1
abhilash singh <110602638+Abhilash4597@users.noreply.github.com>
2023-09-07 19:01:04 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Field from "./components/field";
 import Translate from "./components/translate";
 import Languages from "./components/languages";
+import './styles.css'
 
 export default function App() {
   const [language, setLanguage] = useState("es");
